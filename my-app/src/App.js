83a91942a9e5d522c5f5cbf65db40d3e8812f932
tanyajha16 <jhatanya16aug@gmail.com>
2021-04-04 03:17:13 +0530

    first page added

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+        <div className="bg-text">
+          <h1>
+          APKI SURAKSHA</h1>
+          <p>@copyright TanyaPrakash</p>
+          <div>
+            <button className="loginbutton"><span>LOGIN </span></button>
+          </div>
+          <div>
+            <button className="loginbutton"><span>SIGNUP </span></button>
+          </div>
+          </div>
+      
+    </div>
+  );
+}
+
+export default App;
